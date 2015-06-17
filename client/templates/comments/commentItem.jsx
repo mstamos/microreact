d147aa65d80ce464dd7229fa144da6d44cd9d65e
mstamos <mstamos@gmail.com>
2015-06-17 13:13:09 +0300

    Comments @commentItem.jsx

--- client/templates/comments/commentItem.jsx
+++ client/templates/comments/commentItem.jsx
@@ -1,4 +1,17 @@
+/**
+ * This component render a comment
+ * Props
+ *      author          String      Comment's author
+ *      submittedText   Date        Comment's submitted date
+ *      body            String      Comment's body text
+ */
 CommentItem = React.createClass({
+    //We check the props
+    propTypes: {
+        author: React.PropTypes.string.isRequired,
+        submittedText: React.PropTypes.instanceOf(Date),
+        body: React.PropTypes.string.isRequired,
+    },
     render () {
         return (
             <div>
