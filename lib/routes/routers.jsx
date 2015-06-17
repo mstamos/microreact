75d93efaec949831941d1b49900e162ce724d5ee
mstamos <mstamos@gmail.com>
2015-06-17 08:22:24 +0300

    Before we delete all comments to try new code base

--- lib/routes/routers.jsx
+++ lib/routes/routers.jsx
@@ -1,7 +1,7 @@
 FlowRouter.route('/', {
     name: "postList",
     subscriptions: function (params) {
-        this.register("posts", Meteor.subscribe("posts"))
+        this.register("posts", Meteor.subscribe("posts"));
     },
     action: function () {
         React.render( <PostList />, document.getElementById("yield-section"));
