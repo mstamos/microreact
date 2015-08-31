f44907d19b8632fa1f3832502cd8fc5cc37d9890
mstamos <mstamos@gmail.com>
2015-08-31 17:19:29 +0300

    We separate test definitions for PostSubmit component into two large categories. Logged in users and Logged out users

--- tests/jasmine/client/unit/components/post_submit_spec.js
+++ tests/jasmine/client/unit/components/post_submit_spec.js
@@ -9,45 +9,51 @@ describe("PostSubmit", function () {
         };
     });
 
-    it("should render an input for post's title", function () {
-        // We spyOn Meteor.userId to provide a user id
-        spyOn(Meteor, "userId").and.returnValue("xyz");
-        // We render the component
-        renderWithProps({});
-        //We get the post title children from PostSubmit component
-        var postTitle = post.props.children[0];
+    describe("User is logged in", function () {
+        it("should render an input for post's title", function () {
+            // We spyOn Meteor.userId to provide a user id
+            spyOn(Meteor, "userId").and.returnValue("xyz");
+            // We render the component
+            renderWithProps({});
+            //We get the post title children from PostSubmit component
+            var postTitle = post.props.children[0];
 
-        // We write what is the actual output and what the expected to
-        // make our tests more readable
-        var actual = postTitle.props.title;
-        var expected = "Title";
+            // We write what is the actual output and what the expected to
+            // make our tests more readable
+            var actual = postTitle.props.title;
+            var expected = "Title";
 
-        expect(actual).toBe(expected);
+            expect(actual).toBe(expected);
+        });
+
+        it("should render an input for post's url", function () {
+            // We spyOn Meteor.userId to provide a user id
+            spyOn(Meteor, "userId").and.returnValue("xyz");
+            // We render the component
+            renderWithProps({});
+            // We get the post url children from PostSubmit component
+            var postUrl = post.props.children[1];
+
+            var actual = postUrl.props.title;
+            var expected = "URL";
+
+            expect(actual).toBe(expected);
+        });
     });
 
-    it("should render an input for post's url", function () {
-        // We spyOn Meteor.userId to provide a user id
-        spyOn(Meteor, "userId").and.returnValue("xyz");
-        // We render the component
-        renderWithProps({});
-        // We get the post url children from PostSubmit component
-        var postUrl = post.props.children[1];
+    describe("User is not logged in", function () {
+        it("should render AccessDenied component ", function () {
+            // We spyOn Meteor.userId to provide null userId
+            spyOn(Meteor, "userId").and.returnValue(null);
+            // We render the component
+            renderWithProps({});
 
-        var actual = postUrl.props.title;
-        var expected = "URL";
+            var actual = post.type;
+            var expected = AccessDenied;
 
-        expect(actual).toBe(expected);
+            expect(actual).toBe(expected);
+        });
     });
 
-    it("should render AccessDenied component if there isn't a user(Meteor.userId = null)", function () {
-        // We spyOn Meteor.userId to provide null userId
-        spyOn(Meteor, "userId").and.returnValue(null);
-        // We render the component
-        renderWithProps({});
 
-        var actual = post.type;
-        var expected = AccessDenied;
-
-        expect(actual).toBe(expected);
-    });
 });
