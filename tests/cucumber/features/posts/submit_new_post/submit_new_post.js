f6ab0b5e5467a42dd21ee26ee319369d76b114dc
mstamos <mstamos@gmail.com>
2015-09-25 10:48:54 +0300

    We rebuilt submit_new_post.js for cucumber 0.14

--- tests/cucumber/features/posts/submit_new_post/submit_new_post.js
+++ tests/cucumber/features/posts/submit_new_post/submit_new_post.js
@@ -2,6 +2,7 @@
     'use strict';
 
     module.exports = function () {
+        var actual, expected;
 
         /**
          *   Scenario: Submit a new post
@@ -9,62 +10,60 @@
 
         var url = require('url');
 
-        this.Given(/^I am logged in$/, function (callback) {
+        this.Given(/^I am logged in$/, function () {
 
             // Wait
-            this.client.
-                // We navigate into home page
-                url(process.env.ROOT_URL).
+            // We navigate into home page
+            client.url(process.env.ROOT_URL);
 
-                // Wait for the page to load
-                waitForExist(".container", 1000).
-                waitForVisible(".container", 1000).
+            // Wait for the page to load
+            client.waitForExist(".container", 1000);
+            client.waitForVisible(".container", 1000);
 
-                // We click the login button
-                click("#login-sign-in-link").
-                waitForExist("#login-email").
+            // We click the login button
+            client.click("#login-sign-in-link");
+            client.waitForExist("#login-email");
 
-                // We set the values into email and password
-                setValue("#login-email", "miltos@example.com").
-                setValue("#login-password", "passpass").
+            // We set the values into email and password
+            client.setValue("#login-email", "miltos@example.com");
+            client.setValue("#login-password", "passpass");
 
-                // We click the Sign In button
-                click('#login-buttons-password').
-                call(callback);
+            // We click the Sign In button
+            client.click('#login-buttons-password');
         });
 
         this.Given(/^I navigate to submit new post page$/, function () {
-            return this.client.
-                // We wait for the submit button to exist
-                waitForExist(".submit-post-but").
-                waitForVisible(".submit-post-but").
-                //pause(1000).
-                // We click the submit button
-                click(".submit-post-but").
-                isVisible(".sub-post-but");
+            // We wait for the submit button to exist
+            client.waitForExist(".submit-post-but");
+            client.waitForVisible(".submit-post-but");
+            //pause(1000).
+            // We click the submit button
+            client.click(".submit-post-but");
+            client.isVisible(".sub-post-but");
         });
 
         this.When(/^I fill in all form's fields$/, function () {
             // We just wait for the submit button to exist and then
             // we fill the fields
-            return this.client.
-                waitForExist(".sub-post-but").
-                setValue("#title", "Meteor Point").
-                setValue("#url", "http://www.meteorpoint.com");
+            client.waitForExist(".sub-post-but");
+            client.setValue("#title", "Meteor Point");
+            client.setValue("#url", "http://www.meteorpoint.com");
         });
 
         this.When(/^I submit the form/, function () {
             // We click on submit button
-            return this.client.
-                submitForm(".sub-post-but");
+            client.submitForm(".sub-post-but");
         });
 
         this.Then(/^I should see the new post$/, function () {
             // After the post submission we wait for post-title to exist and check if it is equal with
             // the passing post title from above
-            return this.client.
-                waitForExist(".post-title").
-                getText(".post-title").should.become("Meteor Point");
+            client.waitForExist(".post-title");
+
+            actual = client.getText(".post-title");
+            expected = "Meteor Point";
+
+            expect(actual).toBe(expected);
         });
 
         /**
@@ -72,17 +71,16 @@
          */
 
         this.Given(/^I fill form's fields with existing post$/, function () {
-            return this.client.
-                waitForExist(".sub-post-but").
-                setValue("#title", "Introducing Telescope").
-                setValue("#url", "http://sachagreif.com/introducing-telescope/");
+
+            client.waitForExist(".sub-post-but");
+            client.setValue("#title", "Introducing Telescope");
+            client.setValue("#url", "http://sachagreif.com/introducing-telescope/");
         });
 
         this.Then(/^I should see an error message$/, function () {
             // We check if the error class is visible on the screen
-            return this.client.
-                waitForExist(".error-alert").
-                isVisible(".error-alert");
+            client.waitForExist(".error-alert");
+            client.isVisible(".error-alert");
         });
     }
 
